417b40d618e69d48a491070521bee29802f02e56
Danish-khan93 <danishshoukat1994@gmail.com>
2024-05-28 02:16:58 +0500

    some changes

--- src/component/Gallery.tsx
+++ src/component/Gallery.tsx
@@ -11,19 +11,19 @@ const Gallery = () => {
   return (
     <Box className="px-[20px] w-full flex justify-evenly items-center py-[20px]">
       <Box>
-        <Image src={Pic1} alt="gallery" className="w-[590px]" />
+        <Image src={Pic1} alt="gallery" />
       </Box>
       <Box className="flex flex-col gap-3">
-        <Image src={Pic2} alt="gallery" className="w-[590px]" />
-        <Image src={Pic3} alt="gallery" className="w-[590px]" />
+        <Image src={Pic2} alt="gallery" />
+        <Image src={Pic3} alt="gallery" />
       </Box>
       <Box className="flex flex-col gap-3">
         <Box>
-          <Image src={Pic4} alt="gallery" className="w-[590px]" />
+          <Image src={Pic4} alt="gallery" />
         </Box>
         <Box className="flex justify-between ">
-          <Image src={Pic5} alt="gallery" className="w-[290px]"/>
-          <Image src={Pic6} alt="gallery" className="w-[290px]"/>
+          <Image src={Pic5} alt="gallery" />
+          <Image src={Pic6} alt="gallery" />
         </Box>
       </Box>
     </Box>
